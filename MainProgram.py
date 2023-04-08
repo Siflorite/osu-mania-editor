d58032d2d3181a7d42d4afa7e273ee4e30a9814d
Chenfan Zhang <zcfqq2@163.com>
2023-04-08 19:19:22 +0800

    Add files via upload

---
+++ MainProgram.py
@@ -0,0 +1,239 @@
+import sys
+from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog, QLabel
+from PySide6.QtCore import QStringListModel
+
+from PySide6.QtGui import QPixmap
+from main_ui import Ui_MainWindow
+import os
+import zipfile
+
+from reamber.osu.OsuMap import OsuMap
+from reamber.algorithms.playField import PlayField
+from reamber.algorithms.playField.parts import PFDrawColumnLines, PFDrawBeatLines, PFDrawBpm, PFDrawSv, PFDrawNotes
+
+osuFiles=[]
+active_osu_file = ""
+base_path = ""
+
+bWriteEvents = False
+bWriteTimingPoints = False
+bWriteNotes = False
+misc=[] #osu文件开始时的General,Editor,Metadata,Difficulty
+events=[] #osu文件关于背景动画和声音sample的信息
+timingPoints=[] #osu文件的Timing
+hitObjects=[] #osu文件的HitObjects
+newHitObjects=[]
+
+title = ""
+titleUnicode = ""
+artist = ""
+artistUnicode = ""
+creator = ""
+version = ""
+HP = ""
+Keys = ""
+OD = ""
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent = None):
+        super(MyMainWindow,self).__init__(parent)
+        self.setupUi(self)
+        self.loadButton.clicked.connect(self.loadButtonClicked)
+        self.listView_osulist.clicked.connect(self.osulistClicked)
+        self.generateButton.clicked.connect(self.generateButtonClicked)
+
+    def loadButtonClicked(self):
+        fname = QFileDialog.getOpenFileName(self, "打开osu铺面文件或osz压缩文件", '', "Osu zip files (*.osz);;Osu Beatmap Files (*.osu)", "Osu zip files (*.osz)")
+        self.lineEdit_file.setText(fname[0])
+        typeName = fname[0].split(".")[-1]
+        global osuFiles
+        osuFiles = []
+        if typeName == 'osz':
+            osuFiles = extractOsz(fname[0])
+        else:
+            osuFiles.append(fname[0])
+        if len(osuFiles) > 0:
+            model = QStringListModel()
+            model.setStringList(osuFiles)
+            self.listView_osulist.setModel(model)
+
+    def osulistClicked(self):
+        list_index = self.listView_osulist.currentIndex()
+        list_model = self.listView_osulist.model()
+        global active_osu_file
+        if active_osu_file == list_model.itemData(list_index)[0]:
+            return
+        active_osu_file = list_model.itemData(list_index)[0]
+        global bWriteEvents
+        global bWriteTimingPoints
+        global bWriteNotes
+        bWriteEvents = False
+        bWriteTimingPoints = False
+        bWriteNotes = False
+        analyzeOsuFile(active_osu_file)
+        global misc
+        global events
+        global timingPoints
+        global hitObjects
+
+        global title
+        global titleUnicode
+        global artist
+        global artistUnicode
+        global creator
+        global version
+        global HP
+        global Keys
+        global OD
+        for item in misc:
+            if item.startswith("Title:"):
+                title = item.strip("Title:").strip("\n")
+            elif item.startswith("TitleUnicode:"):
+                titleUnicode = item.strip("TitleUnicode:").strip("\n")
+            elif item.startswith("Artist:"):
+                artist = item.strip("Artist:").strip("\n")
+            elif item.startswith("ArtistUnicode:"):
+                artistUnicode = item.strip("ArtistUnicode:").strip("\n")
+            elif item.startswith("Creator:"):
+                creator = item.strip("Creator:").strip("\n")
+            elif item.startswith("Version:"):
+                version = item.strip("Version:").strip("\n")
+            elif item.startswith("HPDrainRate:"):
+                HP = item.strip("HPDrainRate:").strip("\n")
+            elif item.startswith("CircleSize:"):
+                Keys = item.strip("CircleSize:").strip("\n")
+            elif item.startswith("OverallDifficulty:"):
+                OD = item.strip("OverallDifficulty:").strip("\n")
+        self.updateLineEdit()
+
+    def generateButtonClicked(self):
+        previewPic = generate_preview_pic(active_osu_file)
+        
+        pixmap = QPixmap(str(previewPic))
+        size = pixmap.size()
+        pixmap = pixmap.scaled(size/2)
+
+        self.label_pre = QLabel()
+        self.label_pre.setGeometry(0, 0, pixmap.size().width(), pixmap.size().height())
+        self.label_pre.setPixmap(pixmap)
+        self.label_pre.show()
+
+    def updateLineEdit(self):
+        global title
+        global titleUnicode
+        global artist
+        global artistUnicode
+        global creator
+        global version
+        global HP
+        global Keys
+        global OD
+
+        self.lineEdit_title.setText(title)
+        self.lineEdit_title_unicode.setText(titleUnicode)
+        self.lineEdit_artist.setText(artist)
+        self.lineEdit_artist_unicode.setText(artistUnicode)
+        self.lineEdit_creator.setText(creator)
+        self.lineEdit_version.setText(version)
+        self.lineEdit_hp.setText(HP)
+        self.lineEdit_keys.setText(Keys)
+        self.lineEdit_od.setText(OD)
+
+def extractOsz(FilePath):
+    dirName = os.path.dirname(FilePath)
+    global base_path
+    base_path = dirName
+    osu_file_name = []
+    with zipfile.ZipFile(FilePath, 'r') as osz_zip:
+        for file_name in osz_zip.namelist():
+            if file_name.endswith(".osu"):
+                osu_file_name.append(file_name)
+                osz_zip.extract(file_name, dirName)
+    osu_file_name_withpath = [dirName+'/'+osu_file_name[i] for i in range(len(osu_file_name))]
+    return osu_file_name_withpath
+
+def analyzeOsuFile(FilePath):
+    global misc
+    global events
+    global timingPoints
+    global hitObjects
+    misc = []
+    events = []
+    timingPoints = []
+    hitObjects = []
+    global bWriteEvents
+    global bWriteTimingPoints
+    global bWriteNotes
+    with open(file = FilePath, mode = 'r', encoding = "utf-8") as FileRead:
+        listOfLines = FileRead.readlines()
+        for l in listOfLines:
+            if "[Events]" in l:
+                bWriteEvents = True
+            elif "[TimingPoints]" in l:
+                bWriteEvents = False
+                bWriteTimingPoints = True
+            elif "[HitObjects]" in l:
+                bWriteTimingPoints = False
+                bWriteNotes = True
+            
+            if bWriteEvents:
+                events.append(l)
+            elif bWriteTimingPoints:
+                timingPoints.append(l)
+            elif bWriteNotes:
+                hitObjects.append(l)
+            else:
+                misc.append(l)
+
+def saveOsuFile(FilePath):
+    global misc
+    global events
+    global timingPoints
+    global hitObjects
+
+    global title
+    global titleUnicode
+    global artist
+    global artistUnicode
+    global creator
+    global version
+    global HP
+    global Keys
+    global OD
+    for index,l in enumerate(misc):
+        if "SpecialStyle:" in l:
+            misc[index] = "SpecialStyle:0\n"
+        if "Version:" in l:
+            misc[index] = misc[index].rstrip("\n") + " 6K converted\n"
+        if "CircleSize:" in l:
+            misc[index] = "CircleSize:6\n"
+
+def generate_preview_pic(file):
+    global base_path
+    global title
+    global version
+    m = OsuMap.read_file(str(file))
+    pf = (
+            PlayField(m, padding=30)
+            + PFDrawColumnLines()
+            + PFDrawBeatLines()
+            + PFDrawBpm(x_offset=30, y_offset=0)
+            + PFDrawSv(y_offset=0)
+            + PFDrawNotes()
+    )
+    pf.export_fold(max_height=1000).save(base_path + "/" + title + " " + version +" preview.png")
+    picPath = base_path + "/" + title + " " + version +" preview.png"
+    return picPath
+
+def cleanOsuFile():
+    global osuFiles
+    for extractedOsuFile in osuFiles:
+        os.remove(extractedOsuFile)
+    return
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWin = MyMainWindow()
+    myWin.show()
+    app.aboutToQuit.connect(cleanOsuFile)
+    sys.exit(app.exec())
---
+++ main_ui.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QListView,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.loadButton = QPushButton(self.centralwidget)
+        self.loadButton.setObjectName(u"loadButton")
+        self.loadButton.setGeometry(QRect(20, 10, 75, 24))
+        self.saveButton = QPushButton(self.centralwidget)
+        self.saveButton.setObjectName(u"saveButton")
+        self.saveButton.setGeometry(QRect(110, 10, 75, 24))
+        self.label_title = QLabel(self.centralwidget)
+        self.label_title.setObjectName(u"label_title")
+        self.label_title.setGeometry(QRect(40, 70, 54, 16))
+        self.lineEdit_title = QLineEdit(self.centralwidget)
+        self.lineEdit_title.setObjectName(u"lineEdit_title")
+        self.lineEdit_title.setGeometry(QRect(110, 70, 231, 20))
+        self.lineEdit_title_unicode = QLineEdit(self.centralwidget)
+        self.lineEdit_title_unicode.setObjectName(u"lineEdit_title_unicode")
+        self.lineEdit_title_unicode.setGeometry(QRect(110, 110, 231, 20))
+        self.label_title_unicode = QLabel(self.centralwidget)
+        self.label_title_unicode.setObjectName(u"label_title_unicode")
+        self.label_title_unicode.setGeometry(QRect(13, 110, 81, 20))
+        self.lineEdit_artist = QLineEdit(self.centralwidget)
+        self.lineEdit_artist.setObjectName(u"lineEdit_artist")
+        self.lineEdit_artist.setGeometry(QRect(110, 150, 231, 20))
+        self.label_artist = QLabel(self.centralwidget)
+        self.label_artist.setObjectName(u"label_artist")
+        self.label_artist.setGeometry(QRect(40, 150, 54, 16))
+        self.lineEdit_file = QLineEdit(self.centralwidget)
+        self.lineEdit_file.setObjectName(u"lineEdit_file")
+        self.lineEdit_file.setGeometry(QRect(220, 10, 511, 20))
+        self.listView_osulist = QListView(self.centralwidget)
+        self.listView_osulist.setObjectName(u"listView_osulist")
+        self.listView_osulist.setGeometry(QRect(370, 70, 381, 311))
+        self.label_artist_unicode = QLabel(self.centralwidget)
+        self.label_artist_unicode.setObjectName(u"label_artist_unicode")
+        self.label_artist_unicode.setGeometry(QRect(10, 190, 91, 16))
+        self.lineEdit_artist_unicode = QLineEdit(self.centralwidget)
+        self.lineEdit_artist_unicode.setObjectName(u"lineEdit_artist_unicode")
+        self.lineEdit_artist_unicode.setGeometry(QRect(110, 190, 231, 20))
+        self.lineEdit_creator = QLineEdit(self.centralwidget)
+        self.lineEdit_creator.setObjectName(u"lineEdit_creator")
+        self.lineEdit_creator.setGeometry(QRect(110, 230, 231, 20))
+        self.label_creator = QLabel(self.centralwidget)
+        self.label_creator.setObjectName(u"label_creator")
+        self.label_creator.setGeometry(QRect(30, 230, 91, 16))
+        self.lineEdit_version = QLineEdit(self.centralwidget)
+        self.lineEdit_version.setObjectName(u"lineEdit_version")
+        self.lineEdit_version.setGeometry(QRect(110, 270, 231, 20))
+        self.label_version = QLabel(self.centralwidget)
+        self.label_version.setObjectName(u"label_version")
+        self.label_version.setGeometry(QRect(30, 270, 91, 16))
+        self.label_hp = QLabel(self.centralwidget)
+        self.label_hp.setObjectName(u"label_hp")
+        self.label_hp.setGeometry(QRect(20, 330, 54, 16))
+        self.lineEdit_hp = QLineEdit(self.centralwidget)
+        self.lineEdit_hp.setObjectName(u"lineEdit_hp")
+        self.lineEdit_hp.setGeometry(QRect(50, 330, 71, 20))
+        self.lineEdit_keys = QLineEdit(self.centralwidget)
+        self.lineEdit_keys.setObjectName(u"lineEdit_keys")
+        self.lineEdit_keys.setEnabled(False)
+        self.lineEdit_keys.setGeometry(QRect(170, 330, 71, 20))
+        self.label_keys = QLabel(self.centralwidget)
+        self.label_keys.setObjectName(u"label_keys")
+        self.label_keys.setGeometry(QRect(140, 330, 54, 16))
+        self.lineEdit_od = QLineEdit(self.centralwidget)
+        self.lineEdit_od.setObjectName(u"lineEdit_od")
+        self.lineEdit_od.setGeometry(QRect(280, 330, 71, 20))
+        self.label_od = QLabel(self.centralwidget)
+        self.label_od.setObjectName(u"label_od")
+        self.label_od.setGeometry(QRect(250, 330, 54, 16))
+        self.label_bg = QLabel(self.centralwidget)
+        self.label_bg.setObjectName(u"label_bg")
+        self.label_bg.setGeometry(QRect(30, 370, 321, 201))
+        self.generateButton = QPushButton(self.centralwidget)
+        self.generateButton.setObjectName(u"generateButton")
+        self.generateButton.setGeometry(QRect(370, 390, 75, 24))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"osu!mania chart simple editor", None))
+        self.loadButton.setText(QCoreApplication.translate("MainWindow", u"Load", None))
+        self.saveButton.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.label_title.setText(QCoreApplication.translate("MainWindow", u"Title", None))
+        self.label_title_unicode.setText(QCoreApplication.translate("MainWindow", u"Title(Unicode)", None))
+        self.label_artist.setText(QCoreApplication.translate("MainWindow", u"Artist", None))
+        self.label_artist_unicode.setText(QCoreApplication.translate("MainWindow", u"Artist(Unicode)", None))
+        self.label_creator.setText(QCoreApplication.translate("MainWindow", u"Creator", None))
+        self.label_version.setText(QCoreApplication.translate("MainWindow", u"Version", None))
+        self.label_hp.setText(QCoreApplication.translate("MainWindow", u"HP", None))
+        self.label_keys.setText(QCoreApplication.translate("MainWindow", u"Keys", None))
+        self.label_od.setText(QCoreApplication.translate("MainWindow", u"OD", None))
+        self.label_bg.setText("")
+        self.generateButton.setText(QCoreApplication.translate("MainWindow", u"\u751f\u6210\u9884\u89c8", None))
+    # retranslateUi
+
